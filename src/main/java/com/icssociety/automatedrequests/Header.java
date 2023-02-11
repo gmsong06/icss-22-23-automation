04e247dc1ac68fafddd90251babaa3db63f15d0f
Ronit Barman <ronitbarman493@gmail.com>
2023-02-10 21:41:07 -0800

    add file to set headers to db

---
+++ src/main/java/com/icssociety/automatedrequests/Header.java
@@ -0,0 +1,9 @@
+package com.icssociety.automatedrequests;
+
+import org.javalite.activejdbc.Model;
+import org.javalite.activejdbc.annotations.Table;
+
+@Table("request_headers")
+public class Header extends Model { // the Request model that is stored in the "requests" table
+
+}
