484cc7b69b8fc864c9ad8f2103a6199b1735cae9
kchenturtles <94144671+kchenturtles@users.noreply.github.com>
2023-02-13 10:53:30 -0500

    code to set response headers in table

--- src/main/java/com/icssociety/automatedrequests/Main.java
+++ src/main/java/com/icssociety/automatedrequests/Main.java
@@ -54,6 +54,22 @@ public class Main {
 			
 			request.saveIt(); // saves it to the database
 		}
+
+		for(int i = 0; i < har.getLog().getEntries().size(); i++) { // loops through all the entries of the .har file
+			String req_id = har.getLog().getEntries().get(i).getRequest().getId().toString(); // request id
+			List<HarHeader> headers = har.getLog().getEntries().get(i).getResponse().getHeaders(); // list of response headers
+
+			ResponseHeader response_header = new ResponseHeader(); // creates an instance of the ResponseHeader model
+
+			for(HarHeader h: headers) {
+				response_header.set("id", id);
+				response_header.set("request_id", req_id);
+				response_header.set("name", h.getName());
+				response_header.set("value", h.getValue());
+				response_header.saveIt(); // saves it to the database
+			}
+			
+		}
 		
 		DBConnection.close(); // closes the connection to the database
 		
