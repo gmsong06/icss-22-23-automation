12d444532fcc43157f2ab8d6ef4d386bd41f0c45
FuryFlames <102766292+FuryFlames@users.noreply.github.com>
2023-02-19 21:13:41 -0500

    sending requests, not working

--- src/main/java/com/icssociety/automatedrequests/Request.java
+++ src/main/java/com/icssociety/automatedrequests/Request.java
@@ -1,9 +1,18 @@
 package com.icssociety.automatedrequests;
 
 import org.javalite.activejdbc.Model;
+
+
+
 import org.javalite.activejdbc.annotations.Table;
 
 @Table("requests")
 public class Request extends Model { // the Request model that is stored in the "requests" table
-
+	
+	public void doSomething() {
+		
+		
+		
+	}
+	
 }
