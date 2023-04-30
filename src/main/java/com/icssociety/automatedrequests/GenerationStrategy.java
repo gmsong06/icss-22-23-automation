a0df6d64fd443f530b623dc3c4436f5cd49aa3a7
kchenturtles <94144671+kchenturtles@users.noreply.github.com>
2023-04-29 22:59:10 -0400

    send modified requests

--- src/main/java/com/icssociety/automatedrequests/GenerationStrategy.java
+++ src/main/java/com/icssociety/automatedrequests/GenerationStrategy.java
@@ -1,6 +1,15 @@
 package com.icssociety.automatedrequests;
-import java.math.BigInteger;
+import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.Future;
+
+import com.google.api.client.http.GenericUrl;
+import com.google.api.client.http.HttpHeaders;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpResponse;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.common.util.concurrent.ExecutionError;
 
 public class GenerationStrategy {
 	public static void deleteHeader(Request request, int id) { // removes one header
@@ -10,10 +19,33 @@ public class GenerationStrategy {
 			int headerIndex = i;
 			
 			temp.remove(headerIndex);
-			
+
 			request.setIsGenerated(1);
 			request.setId(null);
 			request.setModification("removed header number " + (headerIndex + 1) + " from the request with id of " + id);
+
+			System.out.println("SENDING MODIFIED REQUEST # " + i);
+
+			HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
+
+			try {
+       		HttpRequest new_request = HTTP_TRANSPORT.createRequestFactory().buildRequest((String) request.getMethod(), new GenericUrl((String) request.getUrl()), null);
+			HttpHeaders new_headers = new HttpHeaders();
+			for(RequestHeader h: temp) {
+				new_headers.set((String)h.getName(), (String) h.getValue());
+			}
+			Future<HttpResponse> response = new_request.setHeaders(new_headers).executeAsync();
+			
+			request.setResponseStatus(response.get().getStatusCode());
+			request.setResponseBody(response.get().parseAsString());
+			System.out.println(response.get().parseAsString());
+			response.get().disconnect();
+			} catch(Exception e) {
+				System.out.println(e.toString());
+			}
+
+			System.out.println("COMPLETED MODIFIED REQUEST # " + i);
+			
 			request.save();
 			
 			for(RequestHeader h : temp) {
